d70c06732a71a941506202e891fc100b6818e0af
Shabab <shababsaifi@gmail.com>
2020-12-08 20:29:00 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-307fed0ef86a089e05ea.js"
+    "url": "webpack-runtime-4f396c851c3efb56a94d.js"
   },
   {
     "url": "framework-dbb498007a7447f28d8e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "8bf39651283fda2868166526399bc3f1"
+    "revision": "fe942ce126b1a81d61b0fab4c5930db6"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "8a385ae9b5c1d5215fc0b71e487784a2"
+    "revision": "e6ca2310a4524507373306e520866b7f"
   },
   {
     "url": "polyfill-ca8adf9770cfeec4c092.js"
